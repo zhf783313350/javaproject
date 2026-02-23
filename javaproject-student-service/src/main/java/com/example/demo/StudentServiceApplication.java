c058a9e578acf9aa1fea33ccd54d904515d1169e
zhhf783313350@qq.com <zhf9207>
2026-02-23 15:29:04 +0800

    升级架构版本

--- javaproject-student-service/src/main/java/com/example/demo/StudentServiceApplication.java
+++ javaproject-student-service/src/main/java/com/example/demo/StudentServiceApplication.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
